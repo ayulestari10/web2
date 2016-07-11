90fdd12109225dcdc552eaea527a53322bb1b47a
ayulestari10 <lestariayu669@gmail.com>
2016-07-12 00:35:31 +0700

    hayy

--- home.php
+++ home.php
@@ -147,108 +147,6 @@
 			background: red;
 		}
 </style>
-</head>
-<body class="yui3-skin-sam" onload="init();">
-
-<div class="big">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="header">
-				<img src="images/depan.jpg" style="height: 150px; margin-right: 15px;" />
-			</div>
-		</div>
-	</div>
-
-	<div class="row">
-		<div class="bigContent">
-			<!-- Sidebar -->
-				<div class="sidebar">
-					<div class="col-md-2">
-						<a href="#">
-							<div class="boxes">
-								Home
-							</div>
-						</a><br>
-						<a href="#">
-							<div class="boxes">
-								Visi Misi				
-							</div>
-						</a><br>
-
-						<a href="#">
-							<div class="boxes" style="height: 50px;">
-								Struktur<br>Organisasi
-							</div>
-						</a><br>
-
-						<a href="#">
-							<div class="boxes">
-								Sejarah
-							</div>
-						</a><br>
-
-
-						<a href="#">
-							<div class="boxes">
-								Pendaftaran
-							</div>
-						</a><br>
-						
-						<a href="#">
-							<div class="boxes">
-								Login
-							</div>
-						</a><br>
-
-						<a href="#">
-							<div class="boxes">
-								Pengumuman
-							</div>
-						</a><br>
-					</div>
-				</div>
-			<!-- End of sidebar -->
-
-			<!-- Content -->
-			<div class="content">
-				<h3>Selamat Datang di Website<br>SMK Muhammadiyah 2 Palembang</h3>
-				<div class="isi">
-					<h5>Syarat-syarat pendaftaran :</h5>
-					<ol>
-						<li>Pendaftaran dimulai tanggal 1 Juni 2016 sampai 10 Juli 2016</li>
-						<li>Nilai Ujian Nasional untuk mata pelajaran Bahasa Inggris, Bahasa Indonesia, Matematika dan IPA minimal 65</li>
-						<li>Mengisi Formulir secara online pada menu Pendaftaran yang ada di website ini</li>
-						<li>Sehat jasmani dan rohani</li>
-						<li>Berasal dari SMP/ MTs Negeri dan Swasta di seluruh Indonesia</li>
-						<li>Mengikuti ujian tertulis pada hari Senin, 11 Juli 2016 dengan membawa nomor ujian</li>
-						<li>Hasil akhir akan diumumkan melalui website ini pada hari Senin, 18 Juli 2016</li>
-					</ol> 
-				</div>
-			</div>
-
-			<!-- End of content -->			
-		</div>
-	</div>
-
-	<!-- Clock & Calendar -->
-	<div class="plugin">
-		<div class="jam" id="clock">
-			
-		</div>
-
-		<div class="kalender" id="mycalendar" style="margin-top: 10%;">
-			
-		</div>
-	</div>
-	<!-- Ended Clock and Calendar-->	
-
-	<div class="row">
-		<div class="footer">
-			<h6>Copyright &copy; 2016 SMK Muhammadiyah 2 Palembang</h6> 
-		</div>
-	</div>
-
-</div>
 
 <script type="text/javascript">
 	new AnalogClock({parentId: 'aclock', milliSecond: true});
@@ -339,6 +237,111 @@
 		}
  		
 </script>
+
+
+</head>
+<body class="yui3-skin-sam" onload="init();">
+
+<div class="big">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="header">
+				<img src="images/depan.jpg" style="height: 150px; margin-right: 15px;" />
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="bigContent">
+			<!-- Sidebar -->
+				<div class="sidebar">
+					<div class="col-md-2">
+						<a href="#">
+							<div class="boxes">
+								Home
+							</div>
+						</a><br>
+						<a href="#">
+							<div class="boxes">
+								Visi Misi				
+							</div>
+						</a><br>
+
+						<a href="#">
+							<div class="boxes" style="height: 50px;">
+								Struktur<br>Organisasi
+							</div>
+						</a><br>
+
+						<a href="#">
+							<div class="boxes">
+								Sejarah
+							</div>
+						</a><br>
+
+
+						<a href="#">
+							<div class="boxes">
+								Pendaftaran
+							</div>
+						</a><br>
+						
+						<a href="#">
+							<div class="boxes">
+								Login
+							</div>
+						</a><br>
+
+						<a href="#">
+							<div class="boxes">
+								Pengumuman
+							</div>
+						</a><br>
+					</div>
+				</div>
+			<!-- End of sidebar -->
+
+			<!-- Content -->
+			<div class="content">
+				<h3>Selamat Datang di Website<br>SMK Muhammadiyah 2 Palembang</h3>
+				<div class="isi">
+					<h5>Syarat-syarat pendaftaran :</h5>
+					<ol>
+						<li>Pendaftaran dimulai tanggal 1 Juni 2016 sampai 10 Juli 2016</li>
+						<li>Nilai Ujian Nasional untuk mata pelajaran Bahasa Inggris, Bahasa Indonesia, Matematika dan IPA minimal 65</li>
+						<li>Mengisi Formulir secara online pada menu Pendaftaran yang ada di website ini</li>
+						<li>Sehat jasmani dan rohani</li>
+						<li>Berasal dari SMP/ MTs Negeri dan Swasta di seluruh Indonesia</li>
+						<li>Mengikuti ujian tertulis pada hari Senin, 11 Juli 2016 dengan membawa nomor ujian</li>
+						<li>Hasil akhir akan diumumkan melalui website ini pada hari Senin, 18 Juli 2016</li>
+					</ol> 
+				</div>
+			</div>
+
+			<!-- End of content -->			
+		</div>
+	</div>
+
+	<!-- Clock & Calendar -->
+	<div class="plugin">
+		<div id="clock">
+			
+		</div>
+
+		<div class="kalender" id="mycalendar" style="margin-top: 30%;">
+			
+		</div>
+	</div>
+	<!-- Ended Clock and Calendar-->	
+
+	<div class="row">
+		<div class="footer">
+			<h6>Copyright &copy; 2016 SMK Muhammadiyah 2 Palembang</h6> 
+		</div>
+	</div>
+
+</div>
+
 
 <script type="text/javascript">
 	YUI().use('calendar', function (Y) {
